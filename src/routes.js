fcb009547e2eb103e2050985eb2a26cfaa785b64
Hudson Tadeu <hudinvesting@gmail.com>
2022-12-27 14:53:56 -0300

    Criação pagina inicial com login e senha

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 
 import HelloControler from "./controllers/HelloControler";
+import UsersController from "./controllers/UsersController";
 
 const routes = new Router();
 
 routes.get('/hello', HelloControler.index);
+
+//REST
+routes.get('/users', UsersController.index);
 
 export default routes;
