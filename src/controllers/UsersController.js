56aa918f6a0c4f5d89659e37df1e9731c2e6cece
Hudson Tadeu <hudinvesting@gmail.com>
2022-12-28 13:23:55 -0300

    CRUD totalmente implementado no projeto

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -13,9 +13,18 @@ class UsersController {
   }
   async show(req, res) {
     try {
+      const { id } = req.params;
+      const user = await User.findById(id);
+
+      if (!user) {
+        return res.status(404).json();
+      }
+
+      return res.json(user);
 
     } catch (err) {
-
+      console.error(err);
+      return res.status(500).json({ error: "Internal server error." });
     }
   }
   async create(req, res) {
@@ -30,7 +39,7 @@ class UsersController {
           .json({ message: 'User ${email} already exists.' });
       }
 
-      const encryptedPassword = await createPasswordHash(password)
+      const encryptedPassword = await createPasswordHash(password);
 
       const newUser = await User.create({ 
         email, 
@@ -45,16 +54,40 @@ class UsersController {
   }
   async update(req, res) {
     try {
+      const { id } = req.params;
+      const { email, password } = req.body;
 
+      const user = await User.findById (id);
+
+      if(!user) {
+        return res.status(404).json();
+      }
+
+      const encryptedPassword = await createPasswordHash(password)
+
+      await user.updateOne({ email, password: encryptedPassword });
+
+      return res.status(200).json();
     } catch (err) {
-
+      console.error(err);
+      return res.status(500).json({ error: "Internal server error." });
     }
   }
   async destroy(req, res) {
     try {
+      const { id } = req.params;
+      const user = await User.findById(id);
 
+      if(!user) {
+        return res.status(404).json();
+      }
+
+      await user.deleteOne();
+
+      return res.status(200).json();
     } catch (err) {
-
+      console.error(err);
+      return res.status(500).json({ error: "Internal server error." });
     }
   }
 }
